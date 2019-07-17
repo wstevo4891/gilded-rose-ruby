339d5ffcdc3e4e212a284705aebeeea0fe847d45
Eric Stephenson <ericb.stephenson@gmail.com>
2019-07-16 23:33:59 -0700

    Complete refactoring of Gilded Rose

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,5 +1,6 @@
 # gilded_rose.rb
 
+# A something that updates item quality and sell_in
 class GildedRose
   BRIE = 'Aged Brie'.freeze
   SULFURAS = 'Sulfuras, Hand of Ragnaros'.freeze
@@ -17,95 +18,119 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      # =====================
-      # BRIE
-      # =====================
-      # if item.name == BRIE
-      #   if item.quality < 50
-      #     item.add_quality
-      #   end
-      #
-      #   item.subtract_sell_in
-      #
-      #   next unless item.sell_in.negative?
-      #
-      #   if item.quality < 50
-      #     item.add_quality
-      #   end
-      # end
-
-      # =====================
-      # BACKSTAGE
-      # =====================
-      # if item.name == BACKSTAGE
-      #   if item.quality < 50
-      #     item.add_quality
-      #     if item.sell_in < 11 && item.quality < 50
-      #       item.add_quality
-      #     end
-      #   end
-      #
-      #   item.subtract_sell_in
-      #
-      #   next unless item.sell_in.negative?
-      #
-      #   item.negate_quality
-      # end
-
-      # =====================
-      # SULFURAS
-      # =====================
-      # if item.name == SULFURAS
-      #   # Absolutely nothing happens!
-      # end
-
-      # =====================
-      # Foo
-      # =====================
-      # if item.name == 'Foo'
-      #   if item.quality.positive?
-      #     item.subtract_quality
-      #   end
-
-      #   item.subtract_sell_in
-
-      #   next unless item.sell_in.negative?
-
-      #   if item.quality.positive?
-      #     item.subtract_quality
-      #   end
-      # end
-
       next if item.name == SULFURAS
 
-      if item.name != BRIE && item.name != BACKSTAGE
-        if item.quality.positive?
-          item.subtract_quality if item.name != SULFURAS
-        end
-      elsif item.quality < 50
-        item.add_quality
-
-        if item.name == BACKSTAGE
-          if item.sell_in < 11 && item.quality < 50
-            item.add_quality
-          end
-        end
-      end
-
-      item.subtract_sell_in if item.name != SULFURAS
-
-      next unless item.sell_in.negative?
-
-      if item.name != BRIE
-        if item.name == BACKSTAGE
-          item.negate_quality
-        elsif item.quality.positive? && item.name != SULFURAS
-          item.subtract_quality
-        end
-      elsif item.quality < 50
-        item.add_quality
-      end
-
+      eval_name(item)
     end
   end
+
+  def eval_name(item)
+    case item.name
+    when BRIE
+      update_brie(item)
+    when BACKSTAGE
+      update_backstage(item)
+    else
+      update_anonymous(item)
+    end
+  end
+
+  def update_brie(item)
+    item.add_quality if item.quality < 50
+
+    item.subtract_sell_in
+
+    return unless item.sell_in.negative?
+
+    item.add_quality if item.quality < 50
+  end
+
+  def update_backstage(item)
+    if item.quality < 50
+      if item.sell_in < 11
+        item.quality += 2
+      else
+        item.add_quality
+      end
+    end
+
+    item.subtract_sell_in
+
+    return unless item.sell_in.negative?
+
+    item.negate_quality
+  end
+
+  def update_anonymous(item)
+    item.subtract_quality if item.quality.positive?
+
+    item.subtract_sell_in
+
+    return unless item.sell_in.negative?
+
+    item.subtract_quality if item.quality.positive?
+  end
 end
+
+##
+# NOTES
+#
+# =====================
+# BRIE
+# =====================
+# if item.name == BRIE
+#   if item.quality < 50
+#     item.add_quality
+#   end
+#
+#   item.subtract_sell_in
+#
+#   next unless item.sell_in.negative?
+#
+#   if item.quality < 50
+#     item.add_quality
+#   end
+# end
+
+# =====================
+# BACKSTAGE
+# =====================
+# if item.name == BACKSTAGE
+#   if item.quality < 50
+#     item.add_quality
+#
+#     if item.sell_in < 11
+#       item.add_quality
+#     end
+#   end
+#
+#   item.subtract_sell_in
+#
+#   next unless item.sell_in.negative?
+#
+#   item.negate_quality
+# end
+
+# =====================
+# SULFURAS
+# =====================
+# if item.name == SULFURAS
+#   # Absolutely nothing happens!
+# end
+
+# =====================
+# Foo
+# =====================
+# if item.name == 'Foo'
+#   if item.quality.positive?
+#     item.subtract_quality
+#   end
+#
+#   item.subtract_sell_in
+#
+#   next unless item.sell_in.negative?
+#
+#   if item.quality.positive?
+#     item.subtract_quality
+#   end
+# end
