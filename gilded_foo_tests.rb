25f380bc918f7a8153bcd24baefaecef2a6cb7cf
Eric Stephenson <ericb.stephenson@gmail.com>
2019-07-16 21:22:57 -0700

    Extract gilded brie and gilded foo tests

---
+++ gilded_foo_tests.rb
@@ -0,0 +1,37 @@
+# gilded_foo_tests.rb
+
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+##
+# Test the GildedRose on Items with a name that
+# is not one of the three constants
+#
+class GildedFooTests < GildedRoseTest
+  def test_foo_quality_zero
+    puts __method__
+    item = gilded_item('Foo', 0, 0)
+
+    assert_equal 0, item.quality
+  end
+
+  def test_foo_quality_positive
+    puts __method__
+    item = gilded_item('Foo', 0, 5)
+
+    assert_equal 3, item.quality
+  end
+
+  def test_foo_sell_in_zero
+    puts __method__
+    item = gilded_item('Foo', 0, 0)
+
+    assert_equal(-1, item.sell_in)
+  end
+
+  def test_foo_sell_in_positive
+    puts __method__
+    item = gilded_item('Foo', 12, 0)
+
+    assert_equal(11, item.sell_in)
+  end
+end
--- test_helper.rb
+++ test_helper.rb
@@ -1,10 +1,21 @@
+# test_helper.rb
+
 require File.join(File.dirname(__FILE__), 'item')
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 require 'test/unit'
 
-def gilded_item(name, sell_in, quality)
-  items = [Item.new(name, sell_in, quality)]
-  GildedRose.new(items).call
-  puts items.first.inspect
-  items.first
+# Parent Class for all Gilded Rose Tests
+class GildedRoseTest < Test::Unit::TestCase
+  def teardown
+    puts ''
+  end
+
+  private
+
+  def gilded_item(name, sell_in, quality)
+    items = [Item.new(name, sell_in, quality)]
+    GildedRose.new(items).call
+    puts items.first.inspect
+    items.first
+  end
 end
